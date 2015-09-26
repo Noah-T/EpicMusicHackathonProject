b169f3119bfec14b87a54a350aab4a3282590ef6
mghamsari <mghamsari@bandsintown.com>
2015-09-26 16:53:57 -0400

    add dbConn

--- record/index.php
+++ record/index.php
@@ -2,7 +2,7 @@
 
 echo 'hi';
 
-include(dbConn.php);
+include(../dbConn.php);
 
 $db = mysqli_connect ($db_host, $db_user, $db_password, $db_name) OR die ('Could not connect to MySQL: ' . mysqli_connect_error());
 
